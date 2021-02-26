80a8b96c13f9f4205cf1174aad2f8f85296cb508
Adrian Orłów <adrian@orlow.me>
2021-02-26 11:38:37 +0100

    changes

--- api/talk/models/talk.js
+++ api/talk/models/talk.js
@@ -1,8 +1,72 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
+const axios = require('axios');
 
-module.exports = {};
+/*const brands = {
+  'mvsp': '6376486378668032',
+  'classic': '5990601388720128',
+  'interview': '4976477451321344',
+  'expert': '6570354065801216',
+  'ring': '6040516632510464'
+};
+
+const destinations = {
+  'facebook': '5451405091667968',
+  'youtube': '5768594248171520'
+};
+
+const headers = {
+  Cookie: `jwt=${process.env.STREAMYARD_JWT}`,
+};
+
+const buildTitle = (data) => {
+
+  const config = {
+    'mvsp': `MŁODZIEŻ VS ${data.title.toUpperCase()}`,
+    'classic': `${data.title} | DEBATA MŁODZIEŻÓWEK`,
+    'interview': `${data.title} o sytuacji w Polsce! | WYWIAD`,
+    'expert': `${data.title} | OKIEM EKSPERTA`,
+    'ring': `${data.title} | RING POLITYCZNY`,
+  }
+
+  return config[data.type];
+}
+
+const createBroadcast = async (data) => {
+  return await axios.request({
+    url: "https://streamyard.com/api/broadcasts",
+    data: {
+      title: buildTitle(data),
+      recordOnly: false,
+      selectedBrandId: brands[data.type],
+      csrfToken: "46Tc0fhhdD7B49nWurLlz8M0"
+    },
+    method: 'POST',
+    headers,
+    withCredentials: true,
+  });
+};
+
+const createOutput = async (data, id, outputType) => {
+  const formData = new FormData();
+
+  formData.append('title', buildTitle(data));
+  formData.append('description', '');
+  formData.append('destinationId', destinations[outputType]);
+
+  if (outputType === 'youtube') {
+    formData.append('image', data.thumbnail);
+    formData.append('plannedStartTime', data.start);
+  }
+
+  return await axios.request({
+    url: `https://streamyard.com/api/broadcasts/${id}/outputs`,
+    data: formData,
+    method: 'POST',
+    headers,
+    withCredentials: true,
+  })
+}*/
+
+module.exports = {
+};
